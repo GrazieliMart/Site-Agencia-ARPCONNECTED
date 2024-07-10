f8e83d7e2ce5ed43ec2426991b5c59f6fd4375cf
Grazieli <grazielimartins5@gmail.com>
2024-07-10 14:18:07 -0300

    aa

--- js/about-parallax.js
+++ js/about-parallax.js
@@ -1,6 +1,9 @@
 window.addEventListener('scroll', function() {
-    const parallax = document.querySelector('.parallax');
-    let scrollPosition = window.pageYOffset;
-  
-    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
-  });
+  const parallax = document.querySelector('.parallax');
+  let scrollPosition = window.pageYOffset;
+
+  // Verifica se a largura da janela é maior que 768px e se não é um dispositivo móvel
+  if (window.innerWidth > 768 && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+  }
+});
